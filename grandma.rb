bfcca47c4689831d1864c5757e281bcccd5c9b4e
Gregory Losco <gtlosco@gmail.com>
2019-03-15 20:59:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
-  if phrase = phrase.downcase()
+  if phrase !!= phrase.downcase()
     return "HUH?! SPEAK UP, SONNY!"
     else 
     "NO, NOT SINCE 1938!"
